e20a24d929ae780a6e97734b6355bdc5d2b37c48
bhw <wyhoys92@gmail.com>
2022-04-13 16:25:55 +0900

    2022-04-13 JAVA_CH01_updated

--- src/com/javaex/ex01/Ex04.java
+++ src/com/javaex/ex01/Ex04.java
@@ -1,7 +1,5 @@
-/*
-작성자 : 방희원
-작성일 : 2022-04-13
-*/
+// 작성자 : 방희원
+// 작성일 : 2022-04-13
 
 //오늘의 수업은 8가지 기본자료형 종류 소개 + Spring (나중에 더 배울 예정)
 
@@ -9,22 +7,22 @@
  * 논리형 : boolean [1byte] use <true> or <false> / e.g. boolean ooo = true;
  * 문자형 : char [2byte] character / 모든유니코드 문자 / e.g. char ooo = 'f';
  * 정수형 : byte [1byte] -128 ~ +127 (0 is counted) / e.g. byte ooo = 89;
- *  	  short [2byte] -32,769 ~ +32,767 / e.g. short ooo = 32760
- *        int  [4byte] -2147483648 ~ +2147483647 / int ooo = 59;
- *        long [8byte] -9223372036854775808 ~ +9223372036854775807 (end with L) / long ooo = 345678912345L;
+  	  	  short [2byte] -32,769 ~ +32,767 / e.g. short ooo = 32760
+   	  	  int  [4byte] -2147483648 ~ +2147483647 / int ooo = 59;
+   	  	  long [8byte] -9223372036854775808 ~ +9223372036854775807 (end with L) / long ooo = 345678912345L;
  * 실수형 : float [4byte] -3.4E38 ~ 3.4E38 (end with F) / float ooo = 32.5F
- * 		  double [8byte] -1.7E308 ~ 1.7E308 / double ooo = 23.34
- * 
- * Spring ooo = "안녕하세요"; (must include "")
+   	  	   double [8byte] -1.7E308 ~ 1.7E308 / double ooo = 23.34
+ 
+ ** Spring ooo = "안녕하세요"; (must include "") / 나중에 더 배울 예정 **
 */
 
+/////////////////////////////////////////////////////////////////////////
+						// JAVA_EX01_2022-04-13 \\
 /////////////////////////////////////////////////////////////////////////
 
 package com.javaex.ex01;
 
 public class Ex04 {
-
-/////////////////////////////////////////////////////////////////////////
 	
 	public static void main (String [] args) {
 	
